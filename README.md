caec71b7928f18308829969ccbb335f431e1c086
ImKrach <rachid.debu@yahoo.fr>
2017-10-12 14:08:25 +0200

    ajouts

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# appMusic
+# tp-frontpage
+
+
+## Menu de navigation
+
+Pour ajouter une entrée, vous pouvez insérer un nouvel élément `<li>Mon sous-menu</li>` à l'intérieur du `<ul></ul>`
+```html
+<aside id="header">
+  <nav>
+    <img src="img/logo.png" alt="Logo musique">
+    <ul>
+      <li>Accueil</li>
+      <li>Recherche</li>
+      <li>Mes favoris</li>
+      <li>Mes playlists</li>
+    </ul>
+  </nav>
+</aside>
+```
+
+
+## Page artiste
+
+Cette section représente le contenu de la page affichée à l'écran.
+```html
+<section id="main" class="conteneur">
+```
+
+Pour créer une page artiste, il faut d'abord ouvrir un `<article></article>` dans lequel on va insérer les éléments ci-dessous.
+```html
+<article class="contenu">
+	Ici, on va insérer les éléments ci-dessous (nom de l'artiste, followers, barre d'actions, biographie, discographie) dans des <div> qui possède différentes classes.
+</article>
+```
+
+L'application est actuellement en mesure de fournir des informations sur un artiste :
+
+* Nom de l'artiste
+	* ```html
+		<div class="entete-artiste">
+          <h2 class="nom-artiste">RIDSA</h2>
+          <h4 class="followers-artiste">86,191 followers</h4>
+        </div>
+      ```
+* Nombre de followers (popularité)
+	* ```html
+		<div class="entete-artiste">
+          <h2 class="nom-artiste">RIDSA</h2>
+          <h4 class="followers-artiste">86,191 followers</h4>
+        </div>
+      ```
+* Une barre d'actions : "PLAY" / "FOLLOW" / "Menu contextuel (pour plus d'actions)"
+	* ```html
+		<div class="actions">
+          <button class="btn btn-principal">PLAY</button>
+          <button class="btn btn-secondaire">FOLLOW</button>
+          <button class="btn btn-transparent">&middot; &middot; &middot;</button>
+        </div>
+      ```
+* Une description/biographie de l'artiste
+	* ```html
+		<div class="description-artiste"> ... </div>
+      ```
+* La liste des différents albums (avec ses titres)
+Si vous préférez la pochette de l'album à droite utiliser la classe .pochette-droite (ou .pochette-haut) sur la `<div class="album-artiste">`
+	* ```html
+		<div class="album-artiste">
+          <div class="pochette-album">
+            <img src="img/RIDSA_pochette-album.jpg" alt="RIDSA - Mes histoires">
+          </div>
+          <ul class="titres-album">
+            <li>
+              <span class="numero-titre">1.</span>
+              <span class="titre-titre">Oh mama</span>
+              <span class="duree-titre">3:21</span>
+            </li>
+          </ul>
+      </div>
+    ```
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,109 @@
 $(document).ready(function() {
+  // initialisation du slider
+  initialisationSlider();
 
+  // Click sur l'image d'un slider
+  clickImageSlider();
+
+  // initialisation de la sidebar
+  initialisationSidebar();
+
+  // initialisation du comportement des titres
+  initilisationTitres();
+
+  // Fonction de changement de thème
+  changerTheme();
+});
+
+// Initialisation de l'angle de rotation à 0
+var angleRotation = 0;
+
+// Initialisation de degré de rotation nécessaire
+var degreRotation = 0;
+
+// Initialisation du slider (calcul du nombre d'images et du degré de rotation nécessaire)
+function initialisationSlider() {
+  // On veut connaître le nombre d'images
+  var nombreImages = $('#slider-rotation img').length;
+
+  // Avec le nombre d'images on peut calculer le degré de rotation de chaque image
+  degreRotation = 360 / (nombreImages-1);
+
+  $('#slider-rotation img').each(function(index) {
+    $(this).css('transform', 'rotateY(' + index*degreRotation*(-1) +'deg)');
+  });
+}
+
+// Le paramètre est 'avant' ou 'apres'
+function animerSlider(sens) {
+  // En fonction du bouton cliqué avant/apres
+  if (sens == "avant") {
+    // L'angle est incrémenté de 45
+    angleRotation = angleRotation + degreRotation;
+  } else if (sens == "apres") {
+    // L'angle est décrémenté de 45
+    angleRotation = angleRotation - degreRotation;
+  }
+
+  // On se sert du css pour effectuer la rotation
+  $('#slider-rotation').css("-webkit-transform", "rotateY("+ angleRotation +"deg)");
+  $('#slider-rotation').css("-moz-transform", "rotateY("+ angleRotation +"deg)");
+  $('#slider-rotation').css("transform", "rotateY("+ angleRotation +"deg)");
+}
+
+// Click sur l'image d'un slider déclenche la lecture de ses musiques
+function clickImageSlider() {
+  $('#slider > #slider-rotation > img').on('click', function(event) {
+    // Cible contient l'id de l'album
+    var cible = $(this).data('target');
+
+    // On scroll tranquillement vers l'album ciblé
+    $('html, body').animate({
+        scrollTop: $(cible).offset().top
+    }, 500);
+  });
+}
+
+// Toggle de la sidebar
+function initialisationSidebar() {
+  // Toggle de la sidebar
+  $('#toggle-sidebar').on('click', function(event) {
+
+    $(this).fadeOut(150, function() {
+      $(this).toggleClass('active');
+      $(this).fadeIn(150);
+    });
+
+    var headerLeftToggle = ($('#header')[0].style.left == '-15%') ? '0%' : '-15%';
+    var mainWidthToggle = ($('#main')[0].style.width == '100%') ? '85%' : '100%';
+    var mainMarginLeftToggle = ($('#main')[0].style.marginLeft == '0%') ? '15%' : '0%';
+
+    $('#header').animate({
+      'left':headerLeftToggle
+    }, 250);
+
+    $('#main').animate({
+      'margin-left':mainMarginLeftToggle,
+      'width':mainWidthToggle
+    }, 250);
+
+  });
+
+  // Toggle des playlists
+  $( ".toggle-sousmenu" ).click(function() {
+    var cible = $(this).data('target');
+    $(cible).slideToggle("normal");
+
+    if ($(this).find('i').hasClass('fa-chevron-down')) {
+      $(this).find('i').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+    } else {
+      $(this).find('i').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+    }
+  });
+}
+
+// Comportement des différents titres de musique (survol, click)
+function initilisationTitres() {
   // li : titre (etat off par défaut)
   $('.titres-album > li').data('status', 'off');
 
@@ -43,118 +147,12 @@ $(document).ready(function() {
       }
     }
   );
-
-  // initialisation du slider
-  initialisationSlider();
-
-  // Toggle de la sidebar
-  $('#toggle-sidebar').on('click', function(event) {
-
-    $(this).toggleClass('active');
-
-
-    // $('#header').slideToggle(250, function() {
-    //   //Stuff to do *after* the animation takes place
-    // });
-
-    console.log("$(this)[0] : " + $(this)[0].style.right);
-    console.log("$(this)[0] : " + $(this)[0].style.right);
-    console.log("$('#header')[0] : " + $('#header')[0].style.left);
-    console.log("$('#main')[0] : " + $('#main')[0].style.width);
-    console.log("$('#main')[0] : " + $('#main')[0].style.marginLeft);
-    // console.log(headerLeftToggle);
-    var thisRightToggle = ($(this)[0].style.right == '15px') ? '-15%' : '15px';
-    var thisOpacityToggle = ($(this)[0].style.right == '15px') ? '-15%' : '15px';
-    var headerLeftToggle = ($('#header')[0].style.left == '0%') ? '-15%' : '0%';
-    var mainWidthToggle = ($('#main')[0].style.width == '85%') ? '100%' : '85%';
-    var mainMarginLeftToggle = ($('#main')[0].style.marginLeft == '15%') ? '0%' : '15%';
-
-    $('#header').animate({
-      'left':headerLeftToggle
-    }, 250);
-
-    $('#main').animate({
-      'margin-left':mainMarginLeftToggle,
-      'width':mainWidthToggle
-    }, 250);
-
-    // $('#toggle-sidebar').animate({
-    //   'opacity':'0'
-    // }, 100, function() {
-    //   $(this).()
-    // });
-    //
-    // $('#toggle-sidebar').fadeOut(100, function() {
-    //     $(this).removeClass('on').addClass('off').fadeIn(250);
-    // });
-
-
-
-    // $('#header').slideToggle('fast', function(){
-    //   console.log('slideToggle');
-    //   if ($(this).is(':visible')) {
-    //     console.log('visible');
-    //     // On retrecie le main et ajoute une marge de 15% (la taille de la sidebar qui apparait)
-    //     $('#main').css('margin-left', '15%');
-    //     $('#main').css('width', '85%');
-    //     $('#toggle-sidebar').removeClass('off').addClass('on');
-    //     // $('#toggle-sidebar').prependTo('#main');
-    //     // TODO
-    //     // $('#main').removeClass('off').addClass('on');
-    //   } else {
-    //     console.log('non visible');
-    //     // On agrandit le main, la sidebar est cachée
-    //     $('#main').css('margin-left', '0%');
-    //     $('#main').css('width', '100%');
-    //     $('#toggle-sidebar').removeClass('on').addClass('off');
-    //     // $('#toggle-sidebar').prependTo('body');
-    //     // TODO
-    //     // $('#main').removeClass('on').addClass('off');
-    //   }
-    // });
-
-  });
-
-  // $(window).on('resize', function(event){
-  //   console.log('#test_1 : ' + $('#test_1').width());
-  //   console.log('#test_2 : ' + $('#test_2').width());
-  //   console.log('#test_3 : ' + $('#test_3').width());
-  //   console.log('#test_4 : ' + $('#test_4').width());
-  //   console.log('#test_5 : ' + $('#test_5').width());
-  // });
-});
-
-// Initialisation de l'angle de rotation à 0
-var angleRotation = 0;
-
-// Initialisation de degré de rotation nécessaire
-var degreRotation = 0;
-
-function initialisationSlider() {
-  // On veut connaître le nombre d'images
-  var nombreImages = $('#slider-rotation img').length;
-
-  // Avec le nombre d'images on peut calculer le degré de rotation de chaque image
-  degreRotation = 360 / (nombreImages-1);
-
-  $('#slider-rotation img').each(function(index) {
-    $(this).css('transform', 'rotateY(' + index*degreRotation*(-1) +'deg)');
-  });
 }
 
-// Le paramètre est 'avant' ou 'apres'
-function animerSlider(sens) {
-  // En fonction du bouton cliqué avant/apres
-  if (sens == "avant") {
-    // L'angle est incrémenté de 45
-    angleRotation = angleRotation + degreRotation;
-  } else if (sens == "apres") {
-    // L'angle est décrémenté de 45
-    angleRotation = angleRotation - degreRotation;
-  }
+function changerTheme() {
+  $('.breadcrumbs_style_element').on('click', function(event) {
+    var cibleCSS = $(this).data('target');
 
-  // On se sert du css pour effectuer la rotation
-  $('#slider-rotation').css("-webkit-transform", "rotateY("+ angleRotation +"deg)");
-  $('#slider-rotation').css("-moz-transform", "rotateY("+ angleRotation +"deg)");
-  $('#slider-rotation').css("transform", "rotateY("+ angleRotation +"deg)");
+    $('#cssSource').attr('href', 'stylesheets/'+cibleCSS);
+  });
 }
